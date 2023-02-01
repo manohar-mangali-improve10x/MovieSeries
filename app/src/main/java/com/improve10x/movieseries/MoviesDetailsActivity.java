8fa1ad8b361c653c606c97e897e9aa8a498de787
Manohar <manohar.mangali@improve10x.com>
2023-02-01 11:03:40 +0530

    MovieSeries Data added

---
+++ app/src/main/java/com/improve10x/movieseries/MoviesDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.movieseries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoviesDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_details);
+    }
+}
